87755f6084591ca11ba705dd6554f587b2523316
Gabriel Felipe <gabrielfelipe1014@gmail.com>
2023-06-27 03:23:54 -0300

    Startando!

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste!");
+        }
+        
+    }
+
+}
